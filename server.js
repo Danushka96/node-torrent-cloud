fde18ed49cf95c957e74fa7fa42a18c313158e11
danushka96 <danushkaherath96@gmail.com>
2021-07-14 23:23:21 +0530

    Revert "code refactor to modern format"
    This reverts commit d67352da

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 //load server config
-const config = require("./config.json");
+var config = require("./config.json");
 //apply defaults from env
-for(let k in config) {
+for(var k in config) {
 	if(k in process.env) {
 		config[k] = process.env[k];
 	}
 }
 
 //load submodules
-const search = require("./lib/search");
-const torrents = require("./lib/torrents");
-const backend = require("./lib/backend");
-const ws = require("./lib/ws");
+var search = require("./lib/search");
+var torrents = require("./lib/torrents");
+var backend = require("./lib/backend");
+var ws = require("./lib/ws");
 
-const basicAuth = require('basic-auth-connect');
-const bodyParser = require('body-parser');
+var basicAuth = require('basic-auth-connect');
+var bodyParser = require('body-parser');
 
-const http = require('http');
-const express = require('express');
-const app = express();
-const server = http.createServer(app);
-const port = parseInt(process.argv[2], 10) ||
-			process.env.PORT ||
-			process.env.OPENSHIFT_NODEJS_PORT ||
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var port = parseInt(process.argv[2], 10) ||
+			process.env.PORT || 
+			process.env.OPENSHIFT_NODEJS_PORT || 
 			3000;
-const host = process.env.HOST ||
+var host = process.env.HOST ||
 			process.env.OPENSHIFT_NODEJS_IP ||
 			"0.0.0.0";
 
 //global auth
-const user = process.env.AUTH_USER || 'admin';
-const password = process.env.AUTH_PASSWORD;
+var user = process.env.AUTH_USER || 'admin';
+var password = process.env.AUTH_PASSWORD;
 if(password)
 	app.use(basicAuth(user, password));
 
@@ -43,16 +43,16 @@ app.use(bodyParser.json());
 //convert all of the given module's
 //exposed functions into API endpoints
 function api(name, module) {
-	Object.keys(module).forEach(key => {
-		let fn = module[key];
+	Object.keys(module).forEach(function(key) {
+		var fn = module[key];
 		if(typeof fn !== "function")
 			return;
 		//dont call modules with request/response,
 		//instead call with 'body' and 'callback(err, data)'
-		const endpoint = '/api/'+name+'/'+key;
+		var endpoint = '/api/'+name+'/'+key;
 		// console.log("POST %s -> %s.%s", endpoint, name, key);
-		app.post(endpoint,  (req, res) => {
-			fn(req.body, (err, data) => {
+		app.post(endpoint, function (req, res) {
+			fn(req.body, function(err, data) {
 				if(err) {
 					res.status(400).send(err);
 				} else {
@@ -71,7 +71,7 @@ api('torrents', torrents);
 //custom APIs
 
 //disallow crawling
-app.get('/robots.txt', (req, res) => {
+app.get('/robots.txt', function(req, res) {
 	res.send("User-agent: *\nDisallow: /");
 });
 
@@ -95,10 +95,10 @@ app.get('/robots.txt', (req, res) => {
 //expose static files
 app.use('/', express.static(__dirname + '/static'));
 
-let storedFiles = {};
+var storedFiles = {};
 
 //broadcast state on "update"
-const update = (newFiles) => {
+var update = function(newFiles) {
 	//optionally update stored files
 	if(newFiles)
 		storedFiles = newFiles;
@@ -116,14 +116,14 @@ torrents.on("update", update);
 
 //periodically scan for new stored files
 function list() {
-	backend.list((err, files) => {
+	backend.list(function(err, files) {
 		if(!err) update(files);
 	});
 }
 setInterval(list, 15*60*1000);
 list();
 
-server.on("error", (err) => {
+server.on("error", function(err) {
 	switch(err.code) {
 		case "EADDRINUSE":
 			console.error("Port %s is currently in use", port);
@@ -135,6 +135,6 @@ server.on("error", (err) => {
 });
 
 //listen!
-server.listen(port, host, () => {
+server.listen(port, host, function() {
 	console.log("listening on %s:%s...", host, port);
 });
